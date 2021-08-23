c8251410df2b3d98ae6ba224a07d4f31af45ed17
unknown <duong.ht96@gmail.com>
2021-08-23 22:55:03 +0700

    Alow any

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'posts', views.PostViewSet)
+router.register(r'to', views.PostViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
