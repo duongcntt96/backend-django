aab761b6b5c67ab7aba8db2f53a6bb800c1ee428
unknown <duong.ht96@gmail.com>
2021-08-23 23:14:43 +0700

    Test deploy

--- backend/settings.py
+++ backend/settings.py
@@ -11,11 +11,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
     'restapi',
 ]
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
